e2d9f9591531783729e08de7b0491e80f2e77d15
ymanish1475-del <ymanish1475@gmail.com>
2025-12-24 20:12:20 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,40 @@
+// console.log("manish bhai ka java schript project  ");
+// console.log("I ");
+// console.log("LOVE");
+// console.log("JS ");
+// name="tony stark";
+// age=24
+// price =3000
+// x=null;
+// y= undefined;
+// isFollow= "follower";
+// console.log(name);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+// console.log(y);
+// console.log(isFollow);
+// let name="tony stark";
+// name="manish"
+// const age=24
+//var age =78
+//let age =78
+//age =98
+// price =3000
+// x=null;
+// y= undefined;
+// isFollow= "follower";
+// console.log(name);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+// console.log(y);
+// console.log(isFollow);
+{
+    let a=78;
+    console.log(a);
+}
+{
+    let a=56;
+    console.log(a);
+}
