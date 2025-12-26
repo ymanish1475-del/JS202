ff5621e15b4065f35364336ed10d133304f2779a
ymanish1475-del <ymanish1475@gmail.com>
2025-12-26 21:51:53 +0530

    LEARN SOME ARITHEMATIC OPERATOR

---
+++ AO.js
@@ -0,0 +1,27 @@
+//ARITHEMATIC OPERATIONS
+
+let a=10;
+//a++;
+//a--;
+let b=5;
+// b=b+1;
+// b++;
+b--;
+let d=2;
+let c=a+b;
+console.log("a=",a, "b=" ,b);
+console.log("a+b=" , a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("c =", c);
+console.log("a%b =",a%b);
+console.log("d%b =",d%b);
+// console.log("b%d =",b%d);
+console.log("a**b=",a**b);
+console.log("2**5=",2**5);
+console.log("d**b=",d**b)
+// console.log("a++=",a++);
+// console.log("b++=",b++);
+console.log("a++=",a++);
+console.log("a=",a);
