a9ba0824aaf5cbfb61ab52dd7bb1276f3045f868
ymanish1475-del <ymanish1475@gmail.com>
2025-12-29 16:15:45 +0530

    LEARN ARITHEMATIC OPERATIONS

---
+++ ArithOpera.js
@@ -0,0 +1,66 @@
+// ARITHEMATIC  ASSIGMENT OPERATIOR
+// a=10;
+// b=5;
+// b+=45; //b=b*45
+// a-=4;
+// c=60;
+// c*=5;  //c=c*5
+// d=100;
+// d/=5;
+// e=100;
+// console.log("e=",e/=25);
+// console.log("d=",d);
+// console.log("c=",c);
+
+// console.log("a=",a);
+// console.log(a+=11);
+// console.log("b=",b);
+
+//comparation operators
+// let a=50;
+// let b=100;
+// let c="50"; 
+// let d=50;
+
+//                 //string change to number in js
+//               // string-> number
+// console.log("50==100",a==b);
+// console.log("50!=100",a!=b);
+// console.log("50==50",a==c);
+// console.log("50!=50",a!=c);
+
+// console.log("50!==50=",a!==c);
+// console.log("50!==50=",a!==d);
+
+// let a=10;
+// let b= 4;
+// let c=10;
+// console.log("a<b=",a<b);
+// console.log("a>b=",a>b);
+
+// console.log("a<=c=",a<=c);
+// console.log("a>=c=",a>=c);
+
+// LOGICAL OPERATOR
+
+let a= 10;
+let b=6;
+
+let cond1 = a>b; //true
+let cond2 = a==10; //true
+let cond3 = a!=10;
+
+console.log("cond1 && cond2 ",cond1 && cond2);
+console.log("a>b && a==10= ",a>b && a==10);
+
+console.log("a>b || a==10= ",a>b || a==10); //OR OPERATOR
+
+console.log("cond1 && cond3 ",cond1 && cond3);
+console.log("a>b && a!=10= ",a>b && a!=10);
+
+console.log("a>b || a!=10= ",a>b || a!=10); // DONO MAI SE KOI EK STATE
+console.log("a<b || a==10= ",a<b || a==10); //MENT SAHI HO TO True
+
+console.log("a<!b || a==10= ",a<!b || a==10); //logical not
+
+ console.log("10<5!=", !(10<5));  //logical not
