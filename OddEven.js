837fd878db0e09190c4a1c63a51bdad9c0d66537
ymanish1475-del <ymanish1475@gmail.com>
2025-12-30 19:54:28 +0530

    LEARN FINDING ODD/ EVEN

---
+++ OddEven.js
@@ -0,0 +1,53 @@
+// conditional statement
+// odd even
+
+let mode = "green";
+
+let color;
+
+if(mode=="black"){
+    console.log("white");
+    color="white";
+}
+else if(mode=="white"){
+    console.log("pink");
+    color="pink";
+
+}
+else if (mode=="blue"){
+    console.log("yellow");
+    color="yellow";
+}
+else if(mode=="green"){
+    console.log("orange")
+    color="orange";
+}
+else{
+    console.log("please enter the color sir ");
+}
+
+console.log(color);
+
+// let num =10;
+
+// if(num<18){
+//     console.log(" junior");
+
+// }
+// else if( num>35){
+//     console.log("senior")
+// }
+// else{
+//     console.log("middle")
+// }
+
+// let num=11;
+
+// if(num%2==0){
+//     console.log(num," even hai");
+// }
+// else{
+//     console.log(num," odd hai");
+// }
+
+// syntex -> rules
