d164adbea80d293e68cd14d36ea6e360e69490f5
ymanish1475-del <ymanish1475@gmail.com>
2025-12-31 19:22:08 +0530

    INTROO OF TERNARY OPERATOR

---
+++ TernaryOperator.js
@@ -0,0 +1,13 @@
+// cinditional statement
+// if else likhne ka doosra tarika
+
+let age = 18;
+
+//  let result=age>=18? "adult" : "not adult ";
+
+ let result=age>=18? console.log( "adult") : console.log("not adult ");
+// simple compact  if else
+
+
+//  console.log(result);
+
