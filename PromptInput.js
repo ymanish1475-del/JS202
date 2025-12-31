a29ad530822fa36d854da98e6184e29e4f2f9e79
ymanish1475-del <ymanish1475@gmail.com>
2025-12-31 19:54:38 +0530

    LEARN PROMPT INPUT

---
+++ PromptInput.js
@@ -0,0 +1,14 @@
+ alert("hello!");  //one alert
+
+ let name = prompt("hello");
+
+ console.log(name);
+
+let num = prompt("number");
+
+if(num%5==0){
+    console.log(num," 5 ka multiple hai");
+ }else{
+    console.log(num," 5 ka multiple nahi hai");
+ }
+ console.log(num);
