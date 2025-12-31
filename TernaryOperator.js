5d0f38f0c7a4d980cc7ff99abbd1f489664ce704
ymanish1475-del <ymanish1475@gmail.com>
2025-12-31 19:51:20 +0530

    EDIT PROMT

--- TernaryOperator.js
+++ TernaryOperator.js
@@ -1,13 +1,28 @@
 // cinditional statement
 // if else likhne ka doosra tarika
 
-let age = 18;
+// let age = 18;
 
 //  let result=age>=18? "adult" : "not adult ";
 
- let result=age>=18? console.log( "adult") : console.log("not adult ");
+//  let result=age>=18? console.log( "adult") : console.log("not adult ");
 // simple compact  if else
 
 
-//  console.log(result);
+//  console.log(resu
+
+// alert("hello!");  //one alert
+
+// let name = prompt("hello");
+
+// console.log(name);
+
+let num = prompt("number");
+
+if(num%5==0){
+    console.log(num," 5 ka multiple hai");
+ }else{
+    console.log(num," 5 ka multiple nahi hai");
+ }
+// console.log(num);
 
