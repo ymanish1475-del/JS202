aa2b420e74c72628603752a24fb82dc6dad660a9
ymanish1475-del <ymanish1475@gmail.com>
2026-01-02 10:10:16 +0530

    LEARN FOR LOOP IN JS

---
+++ Loop.js
@@ -0,0 +1,17 @@
+ // for loops
+ 
+ //let name = "manish";
+
+// for(let i=0 ; i<6 ;i++){
+//     console.log("name");  ///6
+
+// }
+
+let sum=0 ;
+
+for(let i=1; i<=100; i++){
+    sum=sum+i;
+
+}
+console.log("sum=" ,sum);
+console.log("loop ended");
