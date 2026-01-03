4fab01257a1b18fb0f349762d225a6e9fbca3da1
ymanish1475-del <ymanish1475@gmail.com>
2026-01-03 20:29:48 +0530

    LEARN IN LOOP

--- Loop.js
+++ Loop.js
@@ -7,11 +7,17 @@
 
 // }
 
-let sum=0 ;
+// let sum=0 ;
 
-for(let i=1; i<=100; i++){
-    sum=sum+i;
+// for(let i=1; i<=100; i++){
+//     sum=sum+i;
 
+// }
+// console.log("sum=" ,sum);
+// console.log("loop ended");
+
+let min=100;
+for(i=0;i<=10;i++){
+    min=min+i;
 }
-console.log("sum=" ,sum);
-console.log("loop ended");
+console.log(min);
