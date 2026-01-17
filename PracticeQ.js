c35156c6a17dd27f455808b3e40097dcb2829b15
ymanish1475-del <ymanish1475@gmail.com>
2026-01-17 21:55:43 +0530

    BASIC MATH PRACTICE Q WITH JS

---
+++ PracticeQ.js
@@ -0,0 +1,9 @@
+
+let number = prompt("enter your number");
+
+if(number%5===0){
+    console.log("multiple");
+}
+else{
+    console.log("not multiple");
+}
--- WhileLoop.js
+++ WhileLoop.js
@@ -1,1 +1,7 @@
 console.log("mmmmm");
+
+let i=1;
+
+while(i<5){
+    console.log(i);
+}
