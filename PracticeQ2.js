6906a4de4162d30c2e7c55880b0239757ad12556
ymanish1475-del <ymanish1475@gmail.com>
2026-01-29 09:29:02 +0530

    practice q on if else

---
+++ PracticeQ2.js
@@ -0,0 +1,20 @@
+let mark= prompt("enterr your mark");
+
+if(mark >=90 && mark<=100){
+    console.log("A")
+}
+if(mark >=70 && mark<=89){
+    console.log("B")
+}
+if(mark >=60 && mark<=69){
+    console.log("C")
+}
+if(mark >=50 && mark<=59){
+    console.log("D")
+}
+if(mark >=0 && mark<=49){
+    console.log("F")
+}
+else{
+    console.log("kaun gola se ho");
+}
