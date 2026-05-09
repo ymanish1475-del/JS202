9643be85ce8533a312c2bc2bb7a992b0f41e0907
ymanish1475-del <ymanish1475@gmail.com>
2026-05-09 13:42:53 +0530

    string

---
+++ basic2/String.js
@@ -0,0 +1,22 @@
+// let str="hi helllo i am manish yadav "
+// console.log(str.length)
+
+// console.log(str[18])
+
+// // for(let i in str){
+// //     cons
+// // }
+// //TEMPLATE LITERAL
+// let sent=`this is a template`;
+
+// console.log(sent + typeof(sent))
+// console.log(typeof(sent))
+
+let obj={
+
+    price:10,
+    name:"clora"
+}
+console.log("the price of ",obj.name ,"is ",obj.price )
+
+console.log(`the price of ${obj.name} is ${obj.price} \n dom = ${986+987}`)  //string interpolation
