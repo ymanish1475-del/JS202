584b0dd94950184e20520fd387107a0ab1e04e32
ymanish1475-del <ymanish1475@gmail.com>
2026-05-09 11:52:48 +0530

    do while and for of loop

---
+++ basic2/DoWhile.js
@@ -0,0 +1,16 @@
+// let i =20;
+
+// do{
+//     console.log("hii"+i)
+//     i--;
+// }while(i>10)
+
+let name = "JavaScript";
+
+let size=0;
+
+for(let i of name){  //iterator->char
+    console.log("i= "+i)
+    size++
+}
+console.log("size = "+size)
---
+++ basic2/new.js
@@ -0,0 +1,5 @@
+
+
+let name = "manish yadava"+ 6733
+
+console.log(name)
